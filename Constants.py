63890ff8fc8873e4452c10c778740db8d313c41c
Treader7 <AymanWael7@protonmail.com>
2025-12-02 14:21:05 +0200

    more restructuring

---
+++ Constants.py
@@ -0,0 +1,8 @@
+### Constants ###
+BMP_Signature = b'BM'
+BMP_HEADER_SIZE = 54
+Supportted_bit_depth = [24, 32]
+
+MAGIC_NUMBER = 0xCAFEBABE  
+Header_size_bits = 96
+max_message_size = 100000000  # 10 MB
